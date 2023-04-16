526204216858d1fb4986f07c1b5ae9bdfa3c51e2
vitorfail <vitor_andrademanoel@hotmail.com>
2023-04-16 11:43:16 -0300

    remocacao de notas

--- src/App.js
+++ src/App.js
@@ -11,16 +11,17 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [ notas, setnotas] = useState([<>
-          <img src={Nota2} onClick={(e) => adicionar_nota(e.currentTarget.id, e.currentTarget.name)} id='2c' name='2' alt='2' className='nota' />
-          <img src={Nota5} onClick={(e) => adicionar_nota(e.currentTarget.id, e.currentTarget.name)} id='5c' name='5' alt='5' className='nota' />
-          <img src={Nota10} onClick={(e) => adicionar_nota(e.currentTarget.id, e.currentTarget.name)} id='10c' name='10' alt='10' className='nota'/>
-          <img src={Nota20} onClick={(e) => adicionar_nota(e.currentTarget.id, e.currentTarget.name)} id='20c' name='20' alt='20' className='nota'/>
-          <img src={Nota50} onClick={(e) => adicionar_nota(e.currentTarget.id, e.currentTarget.name)} id='50c' name='50' alt='50' className='nota'/>
-          <img src={Nota100} onClick={(e) => adicionar_nota(e.currentTarget.id, e.currentTarget.name)} id='100c' name='100' alt='100' className='nota'/>
+          <img src={Nota2} onClick={(e) => remover_nota(e.currentTarget.name, e.currentTarget.id)} id='2c' name='2' alt='2' className='nota' />
+          <img src={Nota5} onClick={(e) => remover_nota(e.currentTarget.name, e.currentTarget.id)} id='5c' name='5' alt='5' className='nota' />
+          <img src={Nota10} onClick={(e) => remover_nota(e.currentTarget.name, e.currentTarget.id)} id='10c' name='10' alt='10' className='nota'/>
+          <img src={Nota20} onClick={(e) => remover_nota(e.currentTarget.name, e.currentTarget.id)} id='20c' name='20' alt='20' className='nota'/>
+          <img src={Nota50} onClick={(e) => remover_nota(e.currentTarget.name, e.currentTarget.id)} id='50c' name='50' alt='50' className='nota'/>
+          <img src={Nota100} onClick={(e) => remover_nota(e.currentTarget.name, e.currentTarget.id)} id='100c' name='100' alt='100' className='nota'/>
   </>])
   const perguntas = [["A conto ficou por R$ 18,00 e o cliente deu um nota de R$ 50,00", 32], 
   ["O preço foi R$ 31,00 e deram uma nota de R$ 100,00", 69],
-  ["O serviço saiu por R$ 7,00 e te entregam um nota de R$ 20,00", 13]]
+  ["O serviço saiu por R$ 7,00 e te entregam um nota de R$ 20,00", 13], 
+  ["O serviço saiu por R$ 32,00 e te entregam duas nota de R$ 20,00", 8]]
   const [ pergunta_principal, setpergunta_principal] = useState('')
   const [ resposta, setresposta] = useState(0)
   const [troco, settroco] = useState(0.00)
@@ -45,7 +46,7 @@ function App() {
     if(nome === '20') n= Nota20
     if(nome === '50') n= Nota50
     if(nome === '100') n= Nota100
-    let element = <img src={n}   onClick={(e) => adicionar_nota(e.currentTarget.id)}  id={r} name={nome}  alt={nome} className='nota'/>
+    let element = <img src={n} onClick={(e) => remover_nota(e.currentTarget.name, e.currentTarget.id)} id={r} name={nome}  alt={nome} className='nota'/>
     setnotas([...notas, element]);
     if(lista_conta[String(nota)] === 0){
       let ele = document.getElementById(nota+'c')
@@ -66,6 +67,7 @@ function App() {
       setdisplay_troco('troco certo')
       setpopup_certo(true)
     }
+    console.log(troco)
   }
   useEffect(() =>{
     reacarregar()
@@ -87,14 +89,32 @@ function App() {
     settroco(0.00)
     setdisplay_troco('troco')
     setnotas([<></>])
-    setnotas([<>
-      <img src={Nota2} style={{top:'inherit'}} onClick={(e) => adicionar_nota(e.currentTarget.id, e.currentTarget.name)} id='2c' name='2' alt='2' className='nota' />
-      <img src={Nota5}  style={{top:'inherit'}} onClick={(e) => adicionar_nota(e.currentTarget.id, e.currentTarget.name)} id='5c' name='5' alt='5' className='nota' />
-      <img src={Nota10}  style={{top:'inherit'}} onClick={(e) => adicionar_nota(e.currentTarget.id, e.currentTarget.name)} id='10c' name='10' alt='10' className='nota'/>
-      <img src={Nota20}   style={{top:'inherit'}}onClick={(e) => adicionar_nota(e.currentTarget.id, e.currentTarget.name)} id='20c' name='20' alt='20' className='nota'/>
-      <img src={Nota50}  style={{top:'inherit'}} onClick={(e) => adicionar_nota(e.currentTarget.id, e.currentTarget.name)} id='50c' name='50' alt='50' className='nota'/>
-      <img src={Nota100}  style={{top:'inherit'}} onClick={(e) => adicionar_nota(e.currentTarget.id, e.currentTarget.name)} id='100c' name='100' alt='100' className='nota'/>
+    setcount2(0)
+    setcount5(0)
+    setcount10(0)
+    setcount20(0)
+    setcount50(0)
+    setcount100(0)
+    setTimeout(() => {
+      setnotas([<>
+      <img src={Nota2} onClick={(e) => remover_nota(e.currentTarget.name, e.currentTarget.id)} id='2c' name='2' alt='2' className='nota' />
+      <img src={Nota5} onClick={(e) => remover_nota(e.currentTarget.name, e.currentTarget.id)} id='5c' name='5' alt='5' className='nota' />
+      <img src={Nota10} onClick={(e) => remover_nota(e.currentTarget.name, e.currentTarget.id)} id='10c' name='10' alt='10' className='nota'/>
+      <img src={Nota20} onClick={(e) => remover_nota(e.currentTarget.name, e.currentTarget.id)} id='20c' name='20' alt='20' className='nota'/>
+      <img src={Nota50} onClick={(e) => remover_nota(e.currentTarget.name, e.currentTarget.id)} id='50c' name='50' alt='50' className='nota'/>
+      <img src={Nota100} onClick={(e) => remover_nota(e.currentTarget.name, e.currentTarget.id)} id='100c' name='100' alt='100' className='nota'/>
     </>])
+
+    }, 1000)
+  }
+  function remover_nota(nota, id){
+    console.log(troco)
+    var novo_troco = troco-parseInt(nota)
+    if(novo_troco < resposta) setdisplay_troco('troco')
+    settroco(novo_troco)
+
+    //var element = document.getElementById(id)
+    //element.remove()
   }
   return (
     <div className="App">
